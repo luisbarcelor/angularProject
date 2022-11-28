1b03828344dc4eba47989bedae8f26e653aef4a1
luisbarcelor <luisbarcelor@hotmail.com>
2022-11-29 00:45:23 +0100

    Minor Fix

--- src/app/services/input.service.ts
+++ src/app/services/input.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import {FakeData} from "../interfaces/FakeData";
 
 @Injectable({
   providedIn: 'root'
